2fa846ef6dff17841dad849b2f0f2f1648d227cb
brunolferreira <48563530+brunolferreira@users.noreply.github.com>
2021-01-08 17:05:58 +0000

    added history page home2

---
+++ home2.php
@@ -0,0 +1,160 @@
+
+
+
+
+
+<?php
+
+       $dir = "./images";
+       $images_and_folders = scandir($dir);
+       $folders = array('..', '.', 'folder');
+       $images = array_diff($images_and_folders, $folders);
+       $contador = 0;
+       $Total_img = count($images);
+        
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>BLFERREIRA 3</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+  
+
+  <style>
+  </style>
+ </head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Test</h1>
+  <p>Portal </p> 
+</div>
+  
+  
+<div class="container mt-3">
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+
+
+<!-- Indicators -->
+<ul class="carousel-indicators">
+
+  <?php $contador = 0; ?>
+  <?php foreach($images as $image):  ?>
+    <?php if($contador == 0):?>
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <?php else:?>     
+        <li data-target="#myCarousel" data-slide-to="<?=$contador;?>"></li>
+    <?php endif;?> 
+    <?php $contador++;?>
+        
+
+  <?php endforeach;?>
+
+</ul>
+
+
+
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <?php $contador = 0; ?>
+    <?php foreach($images as $image):  ?>
+      <?php if($contador == 0):?>
+           <div class="carousel-item active">
+      <?php else:?> 
+          <div class="carousel-item">
+      <?php endif;?> 
+          <img src="images/<?=$image;?>" alt="<?=$image;?>" class="img-responsive" width="1100" height="500">
+         </div>
+     <?php $contador++;?>
+ <?php endforeach;?>
+
+
+
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div> 
+
+</div> 
+  
+<?Php 
+ /* 
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <h3>Informações</h3>
+          <p>Informaçõesoes sobre o condominio</p>
+          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div class="col-sm-4">
+          <h3>Dispositivos IoT</h3>
+          <p>Controlo dos dispositivos IoT</p>
+          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div class="col-sm-4">
+          <h3>Column 3</h3>        
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+      </div>
+    </div>
+ */
+?>
+
+<div class="container">
+  <div class="row row-cols-1 row-cols-md-3">
+
+    <div class="card-deck">
+      <div class="card"  >
+        <div class="card-body">
+          <h5 class="card-title">Informações</h5>
+          <p class="card-text">Informaçõesoes sobre o condominio.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="card-deck">
+      <div class="card" >
+        <div class="card-body">
+          <h5 class="card-title">Dispositivos IoT</h5>
+          <p class="card-text">Controlo dos dispositivos IoT.</p>
+          <a href="./home_iot.php" class="btn btn-primary">Open</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="card-deck">
+      <div class="card" >
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</div>
+
+</body>
+</html>
+
